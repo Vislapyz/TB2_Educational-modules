d8e42d0dc1dc55a0085036d8380eafb27f967060
mixail <vislapyz@mail.ru>
2024-09-01 21:23:41 +0300

    создание супер пользователя

---
+++ users/management/commands/csu.py
@@ -0,0 +1,16 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    """Создания superuser"""
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='admin@sky.pro',
+            is_superuser=True,
+            is_staff=True,
+        )
+        user.set_password('123qweQWE')
+        user.save()
